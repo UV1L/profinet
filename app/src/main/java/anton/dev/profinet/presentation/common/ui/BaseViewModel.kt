5d8054c2ee9d2678e241ed23eb0d25bdc0f8f480
Anton Vasyanovich <vasyanovich@tochka.com>
2023-04-09 20:01:40 +0500

    поменял NavEvent

--- app/src/main/java/anton/dev/profinet/presentation/common/ui/BaseViewModel.kt
+++ app/src/main/java/anton/dev/profinet/presentation/common/ui/BaseViewModel.kt
@@ -5,16 +5,22 @@ import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.ViewModel
+import anton.dev.profinet.presentation.common.navigation.NavEvent
+import anton.dev.profinet.presentation.common.navigation.NavEventsHandler
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel : ViewModel(),
     LifecycleObserver,
     LifecycleOwner,
     CoroutineScope {
+
+    @Inject
+    lateinit var eventsHandler: NavEventsHandler
 
     private val lifecycleRegistry by lazy { LifecycleRegistry(this) }
 
@@ -26,4 +32,6 @@ abstract class BaseViewModel : ViewModel(),
     protected open fun handleCoroutineException(exception: Throwable) = Unit
 
     final override fun getLifecycle(): Lifecycle = lifecycleRegistry
+
+    fun postEvent(navEvent: NavEvent) = eventsHandler.postEvent(navEvent)
 }
